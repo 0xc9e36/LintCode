e01bc30daca3d590ce87dca064c99bb31e33bca5
wei.tan <2698143402@qq.com>
2017-09-14 13:00:41 +0800

    Update 66.二叉树的前序遍历.java

--- easy/66.二叉树的前序遍历.java
+++ easy/66.二叉树的前序遍历.java
@@ -16,6 +16,8 @@ public class Solution {
      * @param root: A Tree
      * @return: Preorder in ArrayList which contains node values.
      */
+     
+    // 1. 递归遍历，这种方法很好理解.
     public List<Integer> preorderTraversal(TreeNode root) {
         // write your code here
     	List<Integer> list = new ArrayList();
@@ -29,4 +31,27 @@ public class Solution {
     	dfs(root.left, list);
     	dfs(root.right, list);
     }
+    */
+    
+    // 2. 非递归遍历，用栈模拟
+    public List<Integer> preorderTraversal(TreeNode root) {
+        // write your code here
+    	List<Integer> list = new ArrayList();
+    	if(root == null) return list;
+    	Stack<TreeNode> stack = new Stack();
+    	stack.push(root);
+    	while(stack.size() != 0) {
+    	    TreeNode node = stack.pop();
+    	    list.add(node.val);
+    	    //栈的特点是后进先出，所以右孩子节点要先入栈
+    	    if(node.right != null) {
+    	        stack.push(node.right);
+    	    }
+    	    if(node.left != null) {
+    	        stack.push(node.left);
+    	    }
+    	}
+    	return list;
+     }
+    
 }
